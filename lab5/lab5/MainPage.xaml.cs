cd96c68bbd52314841b33239ac12f5f34a111775
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-04-29 18:19:38 -0500

    Agregar archivos de proyecto.

---
+++ lab5/lab5/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonCode());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DatePickerDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new EditorDemo());
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EditorCode());
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryDemo());
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryCode());
+            };
+            Item8.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new LabelDemo());
+            };
+            Item9.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LabelCode());
+            };
+            Item10.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LocalImage());
+            };
+            Item11.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EmbeddedImage());
+            };
+            
+        }
+    }
+}
